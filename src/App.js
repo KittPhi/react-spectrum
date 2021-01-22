4f02b2848733d3d0b0ee583f24349944d39798d3
Kitt Phi <kphi@acxiom.com>
2021-01-22 11:11:12 -0700

    flex and grid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {
   Provider,
   defaultTheme,
   Flex,
+  Grid,
   Heading,
   Form,
   Picker,
@@ -26,23 +27,39 @@ function App() {
 
       <Provider theme={defaultTheme}>
         <View borderWidth="thin" borderColor="dark" padding="size-200">
-          <Flex width="size-3000" justifyContent="space-around">
-            <View
-              height="size-600"
-              width="size-600"
-              backgroundColor="blue-400"
-            ></View>
-            <View
-              height="size-600"
-              width="size-600"
-              backgroundColor="red-500"
-            ></View>
-            <View
-              height="size-600"
-              width="size-600"
-              backgroundColor="green-600"
-            ></View>
-          </Flex>
+          <Grid
+            flex
+            areas={["header  header", "sidebar content", "footer  footer"]}
+            columns={["1fr", "3fr"]}
+            rows={["size-1000", "auto", "size-1000"]}
+            height="size-6000"
+            gap="size-100"
+          >
+            <View backgroundColor="celery-600" gridArea="header" />
+            <View backgroundColor="blue-600" gridArea="sidebar" />
+            <View backgroundColor="purple-600" gridArea="content" />
+            <View backgroundColor="magenta-600" gridArea="footer">
+              <View borderWidth="thin" borderColor="dark" padding="size-200">
+                <Flex width="size-3000" justifyContent="space-around">
+                  <View
+                    height="size-600"
+                    width="size-600"
+                    backgroundColor="blue-400"
+                  ></View>
+                  <View
+                    height="size-600"
+                    width="size-600"
+                    backgroundColor="red-500"
+                  ></View>
+                  <View
+                    height="size-600"
+                    width="size-600"
+                    backgroundColor="green-600"
+                  ></View>
+                </Flex>
+              </View>
+            </View>
+          </Grid>
         </View>
       </Provider>
     </div>
